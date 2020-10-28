f8b46dcf7e588f0bcf07875c77eeaf7d9db4a1be
Adivyes <Adivyes@gmail.com>
2020-10-28 18:50:56 +0200

    first commit

---
+++ syntax.js
@@ -0,0 +1,25 @@
+// var x = 10;
+// x = 20;
+// x = 30;
+
+// let y = 20;
+// y = 30;
+// y = "hello world";
+
+// const z = 30;
+
+// if (y.length > 3) {
+//     consol.log("it's a long string");
+// }else {
+//     console.log("not that long")
+// }
+
+// for (let i=0; i < 10; i++) {
+//     console.log(i);
+// }
+
+let num = 9123;
+while (num > 0) {
+    console.log(num);
+    num = Math.floor(num / 10);
+}
