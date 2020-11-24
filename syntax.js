30ec19a618e69c2d0e4cef910a4beea0a1c41f08
Adivyes <Adivyes@gmail.com>
2020-11-24 08:48:46 +0200

    init

--- syntax.js
+++ syntax.js
@@ -761,20 +761,18 @@
 //         search(songs, isThisIsTheSongWeLookFor);
 //       }
 
-<<<<<<< HEAD
 //       function isThisIsTheSongWeLookFor(song , searchTerm) {
 //         let inputElement = document.getElementById("songInput");
 //         let searchTerm = inputElement.value;
 //         searchTerm = searchTerm.trim().toLowerCase();
-=======
+
 // let firstDiv = document.getElementsByTagName("div")[0];
 // let secondDiv = document.getElementsByTagName("div")[1];
->>>>>>> 9316f36af36c4b5a7eec4cbaa6d4c57d46d79f38
 
 //         let fullSinger = song.singer.toLowerCase();
 //         let fullName = song.name.toLowerCase();
 
-<<<<<<< HEAD
+
 //         let isFoundSinger = fullSinger.indexOf(searchTerm) != -1;
 //         let isFoundName = fullName.indexOf(searchTerm) != -1;
 
@@ -893,7 +891,7 @@ if(document.getElementById(`NameInput`).value.toLowerCase() == 'adiv'){
         document.getElementById(`NameInput`).style.background = 'red'
         document.getElementById(`Namespan`).innerHTML = 'NOT GOOD'
      return false;   
-=======
+
 // function clickEvent(){
 //         secondDiv.innerHTML = '<div id = "innerId"><h1></h1></div><button id= "repCol" onclick="clickEvent2()">replace color</button><div style = "background-color: lightcoral" ></div> <div style = "background-color: lightgreen"></div> <div style = "background-color: lightseagreen"></div>'  
 //         let innerDiv = document.getElementById("innerId")
@@ -908,7 +906,6 @@ if(document.getElementById(`NameInput`).value.toLowerCase() == 'adiv'){
 //           let threeDivs =  document.getElementById("threediv")
 //           threeDivs
 // }
->>>>>>> 9316f36af36c4b5a7eec4cbaa6d4c57d46d79f38
 
 }
 
@@ -922,8 +919,6 @@ function addEvent(idSerch){
 
 }
 
-<<<<<<< HEAD
-=======
 // .style.color = 'blue'
 // ------------------------------------
 var pos = 0;
@@ -950,6 +945,5 @@ var pos = 0;
             chaNgnameBtn.innerHTML = "Play"
         }
         
->>>>>>> 9316f36af36c4b5a7eec4cbaa6d4c57d46d79f38
 
         
